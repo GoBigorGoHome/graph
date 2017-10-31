f74f272a00022ee40756d9b92cb69a18cd6ee058
zjs <zjsdut@163.com>
2017-10-31 22:27:47 +0800

    订正对Dinic直理解错误的Dinic算法

--- dinic.cpp
+++ dinic.cpp
@@ -1,37 +1,43 @@
+#include <algorithm>
 #include <climits>
-int level[N], cur[N];
-std::queue<int> que;
-bool bfs(int s, int t){
-    memset(level, -1, sizeof level);
-    level[s] = 0, que.push(s);
-    while(que.size()){
-        int u = que.front(); que.pop();
+int q[N], n, m, s, t;
+bool bfs(){
+    memset(level, -1, sizeof(int[n + 1]));
+    int beg = 0, end = 0;
+    level[s] = 0; q[end++] = s;
+    while(beg < end){
+        int u = q[beg++];
         for(int i = head[u]; i != -1; i = E[i].next)
-            if(level[E[i].v] == -1){
+            if(E[i].rc && level[E[i].v] == -1){
                 level[E[i].v] = level[u] + 1;
-                que.push(E[i].v);
+                if(E[i].v == t) return true;
+                q[end++] = E[i].v;
             }
     }
-    return level[t] != -1;
+    return false;
 }
-int dfs(int u, int f, int s, int t){
-    if(u == t) return f;
+using LL = long long;
+LL dfs(int u, LL rc){
+    if(u == t) return rc;
+    LL total = 0;
     for(int &i = cur[u]; i != -1; i = E[i].next)
-        if(level[E[i]].v = level[u] + 1 && E[i].rc){
-            int tmp = dfs(E[i].v, min(f, E[i].rc), s, t);
+        if(level[E[i].v] == level[u] + 1 && E[i].rc > 0){
+            int tmp = dfs(E[i].v, std::min(rc, LL(E[i].rc)));
             if(tmp > 0){
                 E[i].rc -= tmp;
                 E[i ^ 1].rc += tmp;
-                return tmp;
+                total += tmp;
+                rc -= tmp;
+                if(rc == 0) break;
             }
         }
-    return 0;
+    return total;
 }
-int dinic(int s, int t){
-    int ans = 0;
-    while(bfs(s, t)){
-        memcpy(cur, head, sizeof head);
-        for(int f; f = dfs(s, INT_MAX, s, t); ans += f);
+LL dinic(){
+    LL ans = 0;
+    while(bfs()){
+        memcpy(cur, head, sizeof(int[n + 1]));
+        for(LL f; f = dfs(s, LLONG_MAX); ans += f);
     }
     return ans;
 }
--- ek.cpp
+++ ek.cpp
@@ -1,10 +1,10 @@
 #include <climits>
-#include <queue>
+#include <algorithm>
 const int N = 1e5 + 5, M = 1e5 + 5;
 struct Edge{
     int v, rc, next;    //rc: residual capacity
 }E[M * 2];
-int head[N], sz;
+int head[N], sz, n, m, s, t;
 void add_edge(int u, int v, int c){
     E[sz] = {v, c, head[u]};
     head[u] = sz++;
@@ -13,30 +13,29 @@ void add_edge(int u, int v, int c){
 }
 void init(){
     sz = 0;
-    memset(head, -1, sizeof head);
+    memset(head, -1, sizeof(int[n + 1]));
 }
-std::queque<int> que;
-int pre[N];
-int ek(int s, int t){
+int pre[N], q[N];
+int ek(){
     for(int ans = 0; ; ){
-        memset(pre, -1, sizeof pre); que.push(s);
-        while(que.size()){
-            int u = que.front(); que.pop();
+        int beg = 0, end = 0;
+        memset(pre, -1, sizeof(int[n + 1])); q[end++] = s;
+        while(beg < end){
+            int u = q[beg++];
             for(int i = 0; i != -1; i = E[i].next){
                 if(E[i].rc > 0 && pre[E[i].v] == -1)
-                    if(E[i] == t){
+                    if(E[i].v == t){
                         int cp = INT_MAX;
                         for(int j = i; j != -1; j = pre[E[j ^ 1].v])
-                            cp = min(cp, E[j].rc);
+                            cp = std::min(cp, E[j].rc);
                         for(int j = i; j != -1; j = pre[E[j ^ 1].v])
                             E[j].rc -= cp, E[j ^ 1].rc += cp;
                         ans += cp;
-                        for(; que.size(); que.pop());
                         break;
                     }
                     else{
                         pre[E[i].v] = i;
-                        que.push(E[i].v);
+                        q[end++] = E[i].v;
                     }
             }
         }
